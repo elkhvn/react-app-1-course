087a831568f668e4fa7f5f369160ce5e262938d3
elkhvn <elxankhabib@gmail.com>
2024-06-13 01:58:59 +0200

    useState hook to save form data (another approach)

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,33 +1,38 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
+  const [isPerson, SetPerson] = useState({ name: "", age: 0 });
 
-  let person = { name: "", age: 0 };
-
-  let handleSubmit = (event: FormEvent) => {
+  const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) {
-      person.name = nameRef.current.value;
-    }
-    if (ageRef.current) {
-      person.age = parseInt(ageRef.current.value);
-    }
+    console.log(isPerson);
   };
-
   return (
     <form onSubmit={handleSubmit} action="">
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} type="name" className="form-control" id="name" />
+        <input
+          onChange={(event) =>
+            SetPerson({ ...isPerson, name: event.target.value })
+          }
+          type="name"
+          className="form-control"
+          id="name"
+        />
         <div className="mb-3">
           <label htmlFor="age" className="forma-label">
             Age
           </label>
-          <input ref={ageRef} id="age" type="number" className="form-control" />
+          <input
+            onChange={(event) =>
+              SetPerson({ ...isPerson, age: parseInt(event.target.value) })
+            }
+            id="age"
+            type="number"
+            className="form-control"
+          />
         </div>
         <div className="mb-3">
           <button type="submit" className="btn btn-primary">
