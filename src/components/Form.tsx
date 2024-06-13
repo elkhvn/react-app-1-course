3e8d6f9b830cdd30e8530a83f81aac4f00faec1c
elkhvn <elxankhabib@gmail.com>
2024-06-13 04:40:05 +0200

    Schema based validation using "zod"

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,24 @@
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
 
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "Age must be at least 18" }),
+});
 
-interface FormData {
-    name: string;
-    age: number;
-}
+//  create a TypeScript type from the schema
+type FormData = z.infer<typeof schema>;
+
 // Nested destructuring
 const Form = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -22,31 +29,24 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name", { required: true, minLength: 3 })}
+          {...register("name")}
           type="name"
           className="form-control"
           id="name"
         />
 
-        {/* Optional Chaining */}
-        {errors.name?.type === "required" && (
-          <p className="text-danger">The name field is empty</p>
-        )}
-        {errors.name?.type === "minLength" && (
-          <p className="text-danger">
-            The name field should be more than 3 characters
-          </p>
-        )}
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
         <div className="mb-3">
           <label htmlFor="age" className="forma-label">
             Age
           </label>
           <input
-            {...register("age")}
+            {...register("age", { valueAsNumber: true })}
             id="age"
             type="number"
             className="form-control"
           />
+          {errors.age && <p className="text-danger">{errors.age.message}</p>}
         </div>
         <div className="mb-3">
           <button type="submit" className="btn btn-primary">
