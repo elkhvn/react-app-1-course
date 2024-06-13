676a677a2a3553ca67066c58817269d21a1c7004
elkhvn <elxankhabib@gmail.com>
2024-06-13 04:13:52 +0200

    ƒorm validation using useForm

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,20 @@
 import { FieldValues, useForm } from "react-hook-form";
 
+
+interface FormData {
+    name: string;
+    age: number;
+}
+// Nested destructuring
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} action="">
       <div className="mb-3">
@@ -11,11 +22,21 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           type="name"
           className="form-control"
           id="name"
         />
+
+        {/* Optional Chaining */}
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is empty</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">
+            The name field should be more than 3 characters
+          </p>
+        )}
         <div className="mb-3">
           <label htmlFor="age" className="forma-label">
             Age
