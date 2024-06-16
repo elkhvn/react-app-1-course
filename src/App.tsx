ad5ef804a17aa8fc8c324e5a37a846cb09fc06b6
elkhvn <elxankhabib@gmail.com>
2024-06-16 04:00:19 +0200

    Add expense list / expense table to track it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 9, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 8, category: "Utilities" },
+  ]);
+
   return (
-    <>
-    <Form></Form>
-    </>
+    <ExpenseList
+      expenses={expenses}
+      onDelete={(id) =>
+        setExpenses(expenses.filter((expense) => expense.id !== id))
+      }
+    />
   );
 }
 
