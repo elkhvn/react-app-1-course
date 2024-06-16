aeb512d7fa28eef8a5eab3621a9fedeec49827d5
elkhvn <elxankhabib@gmail.com>
2024-06-16 04:51:54 +0200

    Add expense form to add expenses

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+
+export const categories = ["Groceries", "Utilies", "Entertainment"];
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 9, category: "Utilities" },
     { id: 3, description: "ccc", amount: 8, category: "Utilities" },
   ]);
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
+
   return (
-    <ExpenseList
-      expenses={expenses}
-      onDelete={(id) =>
-        setExpenses(expenses.filter((expense) => expense.id !== id))
-      }
-    />
+    <div>
+      <ExpenseForm></ExpenseForm>
+      <ExpenseFilter
+        onSelectCategory={(category) => setSelectedCategory(category)}
+      ></ExpenseFilter>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
+    </div>
   );
 }
 
