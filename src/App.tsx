7967ff41f7219fa309a55e2a6f5319ec1d0b6cfe
elkhvn <elxankhabib@gmail.com>
2024-06-18 03:20:04 +0200

    Cleanup code example

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductList from "./components/ProductList";
 
+const connect = () => console.log("connecting");
+
+const disconnect = () => console.log("disconnecting");
+
 function App() {
-  const [category, setCategory] = useState("");
+  useEffect(()=>{
+    connect();
+
+    return () => disconnect();
+  })
 
   return (
     <div>
-      <select
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category}></ProductList>
     </div>
   );
 }
